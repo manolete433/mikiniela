21dc82f981654190619fd53c9216786cfe7f3ed4
CIPHER\m.vazquez <m.vazquez@cipher-sys.com>
2017-10-20 21:50:11 -0400

    Create app folder

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var express = require("express");
-var index = require('./routes/index');
-var userRoutes = require('./routes/users');
-var login = require('./routes/loginroutes');
+var index = require('./app/routes/index');
+var userRoutes = require('./app/routes/users');
+var login = require('./app/routes/loginroutes');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var flash = require('connect-flash');
 var methodOverride = require("method-override");
 var app = express();
 
+app.set('views', './app/views')
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/app/public"));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -17,29 +17,9 @@ app.use(bodyParser.json());
 // app.use(expressValidator());
 app.use(cookieParser());
 app.use(methodOverride("_method"));
-app.use(flash());
-// app.use(expressSession({secret: 'max', saveUninitialized: false, resave: false}));
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-var router = express.Router();
-// test route
-router.get('/', function (req, res) {
-    res.json({
-        message: 'welcome to our upload module apis'
-    });
-});
 
 app.use('/', index);
-app.use('/api', router);
 app.use('/users', userRoutes);
-
-//route to handle user registration
-// router.post('/register',register.register); //registration
-router.post('/login', login.login) //login
 
 app.listen(process.env.PORT || 3000, process.env.IP, function () {
     console.log("MK Server has started");
