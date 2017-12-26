7225c99bc28c9857ddb5e95d97fb91787b0e0648
CIPHER\m.vazquez <m.vazquez@cipher-sys.com>
2017-12-26 12:17:54 -0500

    Enhance Weeks routes and Show.ejs

--- app/routes/weeks.js
+++ app/routes/weeks.js
@@ -68,18 +68,48 @@ router.post("/", middleware.isLoggedIn, function (req, res, next) {
 //Week Show CONTINUE HEEEEEERE!!
 router.get("/:id", middleware.isLoggedIn, function (req, res) {
     Week.findById(req.params.id, {
-        include: [{
-                model: Game,
-                as: 'games'
-            }
-        ]
+        // include: [{
+        //         model: Game,
+        //         as: 'games'
+        //     }
+        // ]
     }).then((foundWeek) => {
         if (!foundWeek) {
             console.log("Week with ID: " + req.body.id + " not found.");
             res.status(404).send("404!!!!");
         } else {
-            res.status(200).render("weeks/show", {
-                week: foundWeek
+            var games = []; //to be sent to our show.ejs
+            Game.findAll({
+                where: {
+                    weekId: foundWeek.id
+                },
+                include: [{
+                        model: Team,
+                        as: 'awayTeam'
+                    },
+                    {
+                        model: Team,
+                        as: 'homeTeam'
+                    }
+                ]
+            }).then((foundGames) => {
+                foundGames.forEach(function (game) {
+                    games.push({
+                        'id': game.id,
+                        'homeTeamName': game.homeTeam.name,
+                        'homeTeamLogo': game.homeTeam.imageURL,
+                        'awayTeamName': game.awayTeam.name,
+                        'awayTeamLogo': game.awayTeam.imageURL,
+                        'gameDate': game.gameDate
+                    });
+                });
+            }).then(() => {
+                res.status(200).render("weeks/show", {
+                    week: foundWeek,
+                    games: games
+                });
+            }).catch((error) => {
+                res.status(500).send(error);
             });
         }
     }).catch((error) => {
